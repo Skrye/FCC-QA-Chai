226c89b5c0e9be321f949cba87f9f663cbb730e4
Skrye <16295244+Skrye@users.noreply.github.com>
2024-10-05 21:23:10 +0000

    corrected errors for functional test part 3

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -39,12 +39,12 @@ suite('Functional Tests', function () {
         .request(server)
         .keepOpen()
         .put('/travellers')
-        .send({ surname: 'Colombo' })
+        .send({ 'surname': 'Colombo' })
         .end(function (err, res) {
           assert.equal(res.status, 200);
           assert.equal(res.type, application/json);
-          assert.equal(res.body.name, Cristoforo);
-          assert.equal(res.body.surname, Colombo);
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
           done();
         });
     });
@@ -58,8 +58,8 @@ suite('Functional Tests', function () {
         .end(function (err, res) {
           assert.equal(res.status, 200);
           assert.equal(res.type, application.json);
-          assert.equal(res.body.name, Giovanni);
-          assert.equal(res.body.surname, da Verrazzano);
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano');
           done();
         })
     });
