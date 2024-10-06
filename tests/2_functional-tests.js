d70b7710caf28b179659114e5377671e51980fad
Skrye <16295244+Skrye@users.noreply.github.com>
2024-10-05 20:28:59 -0400

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -67,19 +67,15 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
-Browser.site = 'https://3000-skrye-fccqachai-el059aq7rx8.ws-us116.gitpod.io/'
-const { application } = require('express');
-const bodyParser = require('body-parser');
-const Browser = require('zombie');
+Browser.site = 'https://3000-skrye-fccqachai-el059aq7rx8.ws-us116.gitpod.io';
 
 suite('Functional Tests with Zombie.js', function () {
   const browser = new Browser();
-  suiteSetup(function(done) {
-    return browser.visit('/',done);
-  })
   this.timeout(5000);
 
-
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
