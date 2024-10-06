555863752d490bf73b7bfcca6651080cff717a22
Skrye <16295244+Skrye@users.noreply.github.com>
2024-10-05 20:14:12 -0400

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -96,10 +96,10 @@ suite('Functional Tests with Zombie.js', function () {
           browser.assert.text('span#name', 'Cristoforo');
           browser.assert.text('span#surname', 'Colombo');
           browser.assert.elements('span#dates', 1);
-          done();
+          done;
         })
       })
-      done();
+      done;
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
@@ -109,10 +109,10 @@ suite('Functional Tests with Zombie.js', function () {
           browser.assert.text('span#name', 'Amerigo');
           browser.assert.text('span#surname', 'Vespucci');
           browser.assert.elements('span#dates', 1);
-          done();
+          done;
         })
       })
-      done();
+      done;
     });
   });
 });
